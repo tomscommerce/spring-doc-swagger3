a0e4746622c5fac2a4824563307398106068d89d
Tom Turner <tomturner@Toms-Mac-mini.local>
2020-09-25 12:04:34 -0700

    add some manually added learnings

---
+++ initial/src/main/java/com/example/springboot/Book.java
@@ -0,0 +1,50 @@
+package com.example.springboot;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Book {
+
+	private long id;
+
+	@NotBlank
+	@Size(min = 0, max = 20)
+	private String title;
+
+	@NotBlank
+	@Size(min = 0, max = 30)
+	private String author;
+
+	public Book(long id, @NotBlank @Size(min = 0, max = 20) String title,
+			@NotBlank @Size(min = 0, max = 30) String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+}
